121827a4099d497b309916e4c8cb06c4e712607f
Matt Welson <matthew.welson@gmail.com>
2021-05-29 15:23:21 +1200

    feat added cypress - broken

---
+++ gatsby/cypress/integration/home/index.spec.js
@@ -0,0 +1,12 @@
+describe('Home page', () => {
+    beforeEach(() => {
+        cy.visit('/')
+    })
+
+    /*
+     * TODO: make this test work
+     */
+    it('Contains the Question text', () => {
+        cy.findByText(/Do you/).should('include.text', 'you want a North Island or South Island')
+    })
+})
